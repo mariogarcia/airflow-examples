bbb1b552bbec3b9f4ec57d9cf3623ddc2d0d6b3a
Mario G <mario.ggar@gmail.com>
2023-12-21 01:23:10 +0100

    fix: change tutorial code for bash command

--- tutorial.py
+++ tutorial.py
@@ -75,7 +75,7 @@ with DAG(
     # [START basic_task]
     t1 = BashOperator(
         task_id="print_date",
-        bash_command="date",
+        bash_command="ls -l",
     )
 
     t2 = BashOperator(
