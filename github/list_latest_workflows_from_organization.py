21bff2ad436317455f89e06505dc651e1fd87a4d
Mario G <mario.ggar@gmail.com>
2023-12-23 00:05:59 +0100

    feat: adding first github example

---
+++ github/list_latest_workflows_from_organization.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    "list_organization_latest_workflows",
+    default_args = {
+        description="A simple tutorial DAG",
+        schedule=timedelta(days=1)
+        catchup=False,
+        tags=["github"]
+    }
+) as dag:    
+    t1 = BashOperator(task_id="", bash_command="ls -l")
+    t1
