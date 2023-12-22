eeb527d46d2a360d894dcfeef6fa7958457d8df2
Mario G <mario.ggar@gmail.com>
2023-12-23 00:17:20 +0100

    fix: syntaxt error in github workflow list example

--- github/list_latest_workflows_from_organization.py
+++ github/list_latest_workflows_from_organization.py
@@ -4,11 +4,14 @@ from airflow.operators.bash import BashOperator
 with DAG(
     "list_organization_latest_workflows",
     default_args={
-        "description"="A simple tutorial DAG",
-        "schedule"=timedelta(days=1),
-        "catchup"=False,
-        "tags"=["github"]
-    }
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A simple tutorial DAG",
+    schedule=timedelta(days=1),
+    catchup=False,
+    tags=["github"]
 ) as dag:    
     t1 = BashOperator(task_id="", bash_command="ls -l")
     t1
