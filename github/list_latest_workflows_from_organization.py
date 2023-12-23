93d51f2f044798ecfb2134c47632f1527c4199a4
Mario G <mario.ggar@gmail.com>
2023-12-23 01:02:19 +0100

    feat: testing kubernetes pod operator

--- github/list_latest_workflows_from_organization.py
+++ github/list_latest_workflows_from_organization.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 with DAG(
     "list_organization_latest_workflows",
@@ -14,6 +15,15 @@ with DAG(
     start_date=datetime(2023, 12, 23),
     catchup=False,
     tags=["github"]
-) as dag:    
-    t1 = BashOperator(task_id="gh_org_workflow_list", bash_command="ls -l")
-    t1
+) as dag:
+    k = KubernetesPodOperator(
+        name="hello-dry-run",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        do_xcom_push=True,
+    )
+
+    k.dry_run()
